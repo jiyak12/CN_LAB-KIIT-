ecf6b0a07b0143899942e9e96a9392f79226fccf
Jiya <jiyajha2020@gmail.com>
2025-05-16 23:26:00 +0530

    Create client.c

---
+++ LAB-3/PROG-3/client.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <string.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include <unistd.h>
+
+int main() {
+    int sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+    if (sockfd < 0) printf("Socket Creation Failed\n");
+    else printf("Socket Created Successfully\n");
+
+    struct sockaddr_in serverAddr = {
+        .sin_family = AF_INET,
+        .sin_port = htons(8080),
+        .sin_addr.s_addr = 0,
+    };
+    struct sockaddr_in clientAddr;
+    socklen_t clientAddrLen = sizeof(clientAddr);
+
+    int array[10] = {7, 9, 9, 9, 1, 8, 5, 2, 2, 7};
+    
+    int isSent = sendto(sockfd, array, sizeof(array), 0, (struct sockaddr*)&serverAddr, sizeof(serverAddr));
+    if (isSent < 0) printf("Failed to Send\n");
+    else printf("Array Sent Successfully\n");
+
+    int greatest;
+    int isRecv = recvfrom(sockfd, &greatest, sizeof(greatest), 0, (struct sockaddr*)&clientAddr, &clientAddrLen);
+    if (isRecv < 0) printf("Failed to Receive\n");
+    else printf("Greatest Recieved: %d\n", greatest);
+
+    close(sockfd);
+}
